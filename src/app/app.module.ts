ae01899341d1580d60a4188f292ef593c0165ae8
Chaim <cfinson@emergingmed.com>
2020-07-15 21:31:57 +0300

    more initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Material } from './material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { SearchAutocompleteComponent } from './components/search-autocomplete/search-autocomplete.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {TopNavComponent} from './components/top-nav/top-nav.component';
+import {FavoritesComponent} from './views/favorites/favorites.component';
+import {HomeComponent} from './views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchAutocompleteComponent,
+    TopNavComponent,
+    HomeComponent,
+    FavoritesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    Material,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
