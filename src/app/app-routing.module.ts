68a25c275ba09a383b927d49922c8ae47d9539c2
Chaim <cfinson@emergingmed.com>
2020-07-15 21:33:50 +0300

    router setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './views/home/home.component';
+import {FavoritesComponent} from './views/favorites/favorites.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+   {path: '', component: HomeComponent},
+  {path: 'favorites', component: FavoritesComponent}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
