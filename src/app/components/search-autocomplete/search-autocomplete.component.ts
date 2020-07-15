4940229581f5178a383ce7fc0a50d3a93a50637d
Chaim <cfinson@emergingmed.com>
2020-07-15 21:33:39 +0300

    Auto complete component setup

---
+++ src/app/components/search-autocomplete/search-autocomplete.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable, observable, of} from 'rxjs';
+import {
+  startWith,
+  map,
+  debounceTime,
+  switchMap,
+  catchError,
+  tap
+} from 'rxjs/operators';
+import {WeatherService} from '../../services/weather.service';
+import { Cities } from '../../model/interfaces';
+
+@Component({
+  selector: 'app-search-autocomplete',
+  templateUrl: './search-autocomplete.component.html',
+  styleUrls: ['./search-autocomplete.component.css'],
+})
+export class SearchAutocompleteComponent implements OnInit {
+
+  constructor(public weatherService: WeatherService){ }
+  public citiesAutoComplete: Observable<Cities> = null;
+  public myControl = new FormControl();
+
+  lookup(value: string): Observable<Cities> {
+    return this.weatherService.getCities(value.toLowerCase()).pipe(
+      // map the item property of the weather results as our return object
+      tap(res => console.log(res)),
+      // catch errors
+      catchError(_ => {
+        return of(null);
+      })
+    );
+  }
+
+  ngOnInit(): void {
+    this.citiesAutoComplete = this.myControl.valueChanges.pipe(
+      startWith(''),
+      // delay emits
+      debounceTime(300),
+      // use switch map so as to cancel previous subscribed events, before creating new once
+      switchMap(value => {
+        if (value !== '') {
+          // lookup from github
+          return this.lookup(value);
+        } else {
+          // if no value is present, return null
+          return of(null);
+        }
+      })
+    );
+  }
+}
