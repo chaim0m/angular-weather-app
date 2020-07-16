6622b9cd4d7270b9347855308a9411f9975c6461
Chaim <cfinson@emergingmed.com>
2020-07-16 23:02:06 +0300

    bringing data now to home

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {WeatherService} from '../../services/weather.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor(public weatherService: WeatherService){
+    // this.getCityForecast('215854');
+  }
+  days: any;
+  isLoaded: boolean;
+  celcius: boolean = true;
+  getCityForecast(cityKey: string): any{
+    this.isLoaded = false
+    return this.weatherService.get5DaysForecasts(cityKey, true).subscribe(days => {
+      this.days = days.DailyForecasts;
+      this.isLoaded = true;
+      });
+  }
 
   ngOnInit(): void {
+    // this.getCityForecast('215854');
   }
 
 }
