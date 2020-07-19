ef97a695cbfd5ee41f0cf1ca30b414a3f53c5089
Chaim <cfinson@emergingmed.com>
2020-07-19 22:17:48 +0300

    Favorites setup

--- src/app/views/favorites/favorites.component.ts
+++ src/app/views/favorites/favorites.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable, Subscription} from 'rxjs';
+import {FavoritesState} from '../../store/reducers/favorites.reducer';
+import {City} from '../../model/interfaces';
+import { map } from 'rxjs/operators';
+import {WeatherService} from '../../services/weather.service';
+import {UtilsService} from '../../services/utils.service';
+import {select, Store} from '@ngrx/store';
+import * as favoritesActions from '../../store/actions/favorites.actions';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-favorites',
@@ -7,9 +17,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public weatherService: WeatherService, private router: Router, private store: Store<{favorites: FavoritesState}>) {
+    this.favorites$ = store.pipe(select('favorites'));
+  }
+  favorites$: Observable<FavoritesState>;
+  FavoritesSubscription: Subscription;
+  FavoritesList: City[] = [];
 
+   onFavoriteClick(favorite: City): void{
+      const copy = JSON.parse(JSON.stringify(favorite))
+      this.store.dispatch(favoritesActions.setSelectedFavorite({data: copy}));
+      this.router.navigateByUrl('/').then();
+  }
   ngOnInit(): void {
+    this.FavoritesSubscription = this.favorites$
+      .pipe(
+        map(x => {
+          this.FavoritesList = x.favoritesArray;
+        })
+      )
+      .subscribe();
   }
 
 }
