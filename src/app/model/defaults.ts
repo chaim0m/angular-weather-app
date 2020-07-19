db8f48aac6bc914971db58fdcfccfdd40a3ca7a2
Chaim <cfinson@emergingmed.com>
2020-07-19 22:17:10 +0300

    add default

---
+++ src/app/model/defaults.ts
@@ -0,0 +1,14 @@
+import {City} from './interfaces';
+
+export const defaultCity: City = {
+  Key: '215854',
+  LocalizedName: 'Tel Aviv',
+  isFavorite: false,
+  Country: {
+    ID: 'IL',
+    LocalizedName: 'Israel'
+  },
+  WeatherDesc: '',
+  weatherIcon: null,
+  Temperature: null,
+};
